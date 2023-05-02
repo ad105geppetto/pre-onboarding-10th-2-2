4f8f47244f3733699493bf20a68fbb299d73fbcb
ad105geppetto <ad105geppetto@gmail.com>
2023-05-03 02:05:48 +0900

    feat: 검색어 API 요청 함수 추가
    # package.json
    * CORS 에러가 발생하여 CRA의 proxy 옵션을 사용하여 문제 해결
    
    # fetchSearchSuggestions.tsx
    * 검색어 추천을 위한 API 요청 함수 작성

--- src/api/fetchSearchSuggestions.tsx
+++ src/api/fetchSearchSuggestions.tsx
@@ -1,1 +1,12 @@
-export default function fetchSearchSuggestions() {}
+import axios from "axios";
+
+export default async function fetchSearchSuggestions(name: string) {
+  try {
+    const result = await axios.get(`/api/v1/search-conditions/?name=${name}`);
+    return result.data;
+  } catch (error) {
+    if (error instanceof Error) {
+      window.alert(error.message);
+    }
+  }
+}
