dec528d16290b8fb2d891eb6b8670f260d18e06e
ad105geppetto <ad105geppetto@gmail.com>
2023-05-05 21:11:50 +0900

    feat: netlify proxy 설정

--- src/lib/customAxios.ts
+++ src/lib/customAxios.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 import { RESOURCE_PATH } from "../constant";
 
+const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
 const customAxios = axios.create({
-  baseURL: RESOURCE_PATH,
+  baseURL: `${PROXY}${RESOURCE_PATH}`,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default customAxios;
